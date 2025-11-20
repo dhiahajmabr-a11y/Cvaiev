9b47f4a7a57e5d97a78af9e5084d224e39a9108c
Ayoub Kefi <ayoubkefi1@gmail.com>
2025-11-20 19:34:54 +0100

    printed hello pinguin

--- src/pgdp/hello/HelloPenguins.java
+++ src/pgdp/hello/HelloPenguins.java
@@ -6,5 +6,6 @@ public class HelloPenguins {
      * Prints the String "Hallo Pinguine!" to the console
      */
     public static void main(String[] args) {
+        system.out.println("Hallo Pinguine ! ");
     }
 }
