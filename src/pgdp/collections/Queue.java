b7f7a280ce11fe67bc9da027ee17deddf9ff590b
Dhia Haj Mabrouk <Dhiahajmabrouk198@gmail.com>
2025-11-24 21:15:16 +0100

    dghia

---
+++ src/pgdp/collections/Queue.java
@@ -0,0 +1,15 @@
+package pgdp.collections;
+
+public class Queue {
+
+    // TODO: do your programming magic! 🐧
+
+    public String pop() {
+        // TODO: do your programming magic! 🐧
+        return null;
+    }
+
+    public void push(String value) {
+        // TODO: do your programming magic! 🐧
+    }
+}
